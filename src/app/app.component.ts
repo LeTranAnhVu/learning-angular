a0d704231d1e6045cd3ad061e2bdc423c4fd43b3
LeTranAnhVu <vule5726@gmail.com>
2019-01-04 16:01:36 +0700

    lesson

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'learning';
+  ngu: any;
+  onChange(param) {
+    console.log('this is ');
+    console.log(param.value);
+    console.log(typeof param);
+
+  }
+  onChildChange() {
+    console.log("child changed");
+  }
+}
---
+++ src/app/components/child/child.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.sass']
+})
+export class ChildComponent implements OnInit {
+  @Output() change = new EventEmitter();
+  constructor() { }
+  courses = ['angular', 'react', 'nodejs', 'firebase', 'vanilla js'];
+  viewMode = 'something else';
+  newCourse = '';
+  onClick() {
+    this.change.emit();
+  }
+  onAdd() {
+    if (this.newCourse) {
+      this.courses.push(this.newCourse);
+      this.newCourse = '';
+    }
+  }
+  onRemove(whichCourse) {
+    if (confirm("Are you sure to delete " + name)) {
+      console.log("Implement delete functionality here");
+      this.courses = this.courses.filter((course) => {
+        return (course !== whichCourse);
+      })
+    }
+
+  }
+  onEdit(editedCourse) {
+    let newCourseName: string = prompt('Please enter ');
+    this.courses = this.courses.map((course) => {
+      if (course === editedCourse) {
+        course = newCourseName;
+      } else {
+        // do nothing
+      }
+      return course;
+    })
+  }
+  ngOnInit() {
+  }
+  changeView(target) {
+    this.viewMode = target;
+  }
+
+}
