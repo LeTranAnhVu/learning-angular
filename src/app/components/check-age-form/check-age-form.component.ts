b8aa094611a3cd61965d1a7d9fdee894e9dfd6e4
LeTranAnhVu <vule5726@gmail.com>
2019-01-11 09:38:58 +0700

    finish check age

--- src/app/components/check-age-form/check-age-form.component.ts
+++ src/app/components/check-age-form/check-age-form.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-check-age-form',
@@ -6,11 +7,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./check-age-form.component.sass']
 })
 export class CheckAgeFormComponent implements OnInit {
+  @ViewChild('temForm') form;
   age: number;
   birthDay: Date;
+  isOver: boolean;
   constructor() { }
 
   ngOnInit() {
+    console.log(new Date());
+
+  }
+  checkAgeIsOver18() {
+    console.log(this.form);
+    const date: Date = new Date(this.form.form.get('birthDay').value);
+    // console.log('date', date);
+    // console.log('date', typeof date);
+    return this.checkAgeIsOver(18, date);
+  }
+  checkAgeIsOver(limitedAge, date: Date) {
+    const now = new Date();
+    let age: number = now.getFullYear() - date.getFullYear();
+    if (age >= limitedAge + 1) {
+      // age 19
+      age = age;
+    } else if (age >= limitedAge - 1) {
+      // age 17 and 18
+      const m = now.getMonth() - date.getMonth();
+      const d = now.getDate() - date.getDate();
+      if (m < 0 || (m === 0 && d < 0)) {
+        // thiếu tháng thiếu ngày ==> chưa đủ tuổi
+        age--;
+      } else if (m === 0 && d > 0) {
+        // cùng tháng và hơn ngày ==> tăng thêm 1 tuổi
+        age++;
+      } else if (m === 0 && d === 0) {
+        // ngay ngày sinh nhât ==> tuổi giữa nguyên
+        age = age;
+      } else if (m > 0) {
+        // du thang => tăng thêm 1 tuổi
+        age++;
+      }
+    }
+
+    // kq cuoi cung
+    this.age = age;
+    // kiem tra
+    if (this.age >= limitedAge) {
+      this.isOver = true;
+      return true;
+    }
+    this.isOver = false;
+    return false;
   }
 
 }
+
