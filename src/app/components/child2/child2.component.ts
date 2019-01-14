36a5b4e03afc1e3efb587fd1c3ea910e5e6bd1c3
LeTranAnhVu <vule5726@gmail.com>
2019-01-14 15:15:26 +0700

    combineLatest- multiple observable handleing

--- src/app/components/child2/child2.component.ts
+++ src/app/components/child2/child2.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable, combineLatest } from 'rxjs';
+
 
 @Component({
   selector: 'app-child2',
@@ -12,9 +14,24 @@ export class Child2Component implements OnInit, OnDestroy {
 
   ngOnInit() {
     console.log('ngOnInit from child2');
-    this.route.paramMap.subscribe(param => {
-      console.log(param.get('id'));
-    });
+    // this.route.paramMap.subscribe(param => {
+    //   console.log(param.get('id'));
+    // });
+
+    // this.route.queryParamMap.subscribe(param => {
+    //   console.log('query?');
+    //   console.log(param.get('page'));
+    // })
+
+    // subcribe multiple observables at same time
+    combineLatest([
+      this.route.paramMap,
+      this.route.queryParamMap
+    ]).subscribe(combinedResult => {
+      const id = combinedResult[0].get('id'); // paramMap
+      const page = combinedResult[1].get('page'); //queryParamMap
+      console.log('id:', id, '-', 'page:', page);
+    })
 
 
 
